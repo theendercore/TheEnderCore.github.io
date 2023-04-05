2c6da3f424a5e380ae4313462b86b8242305a73c
theendercore <theendercore@gmail.com>
2023-04-05 22:55:38 +0300

    first page design

--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,28 @@
 const StreamLink = "https://www.twitch.tv/theendercore";
+const YouTubeLink = "https://www.youtube.com/c/@TheEnderCore";
+const GitHubLink = "https://github.com/theendercore";
+const TwitterLink = "https://twitter.com/theendercoreoff";
+const ModrinthLink = "https://modrinth.com/user/theendercore";
+const KoFiLink = "https://ko-fi.com/theendercore";
+const DiscordLink = "https://discord.gg/KZ5Q364hEg";
 
-export { StreamLink };
+const LinkList = [
+  { link: YouTubeLink, icon: "youtube", alt: "YouTube" },
+  { link: StreamLink, icon: "twitch", alt: "Twitch" },
+  { link: GitHubLink, icon: "github", alt: "GitHub" },
+  { link: TwitterLink, icon: "twitter", alt: "Twitter" },
+  { link: ModrinthLink, icon: "modrinth", alt: "Modrinth" },
+  { link: KoFiLink, icon: "ko-fi", alt: "Ko-fi" },
+  { link: DiscordLink, icon: "discord", alt: "Discord" },
+];
+export {
+  StreamLink,
+  YouTubeLink,
+  GitHubLink,
+  TwitterLink,
+  ModrinthLink,
+  KoFiLink,
+  DiscordLink,
+  LinkList,
+};
+ 
